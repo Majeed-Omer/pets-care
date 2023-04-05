ce280300e767621e3dc1279ced4050246285f69b
Majeed-Omer <twom764@gmail.com>
2023-04-05 15:40:34 +0300

    m

--- resources/views/pages/home/home.blade.php
+++ resources/views/pages/home/home.blade.php
@@ -30,28 +30,6 @@
 
   <br><br><br><br>
 
-<!-- <div class="row">
-  <div class="column">
-    <img src="images/cat.jpg" alt="Snow" style="width:100%">
-    <img src="images/cat.jpg" alt="Snow" style="width:100%">
-  </div>
-  <div class="column">
-    <img src="images/cat.jpg" alt="Forest" style="width:100%">
-    <img src="images/cat.jpg" alt="Snow" style="width:100%">
-  </div>
-  <div class="column">
-    <img src="images/cat.jpg" alt="Mountains" style="width:100%">
-    <img src="images/cat.jpg" alt="Snow" style="width:100%">
-  </div>
-  <div class="column">
-    <img src="images/cat.jpg" alt="Forest" style="width:100%">
-    <img src="images/cat.jpg" alt="Snow" style="width:100%">
-  </div>
-  <div class="column">
-    <img src="images/cat.jpg" alt="Mountains" style="width:100%">
-    <img src="images/cat.jpg" alt="Snow" style="width:100%">
-  </div>
-</div> -->
 @yield('content')
 <br><br><br><br><br>
 </body>
