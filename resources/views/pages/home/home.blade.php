bfb5e73442260c19439f205b94c922ff3e0d5e90
Majeed-Omer <twom764@gmail.com>
2023-04-16 02:35:28 +0300

    m

--- resources/views/pages/home/home.blade.php
+++ resources/views/pages/home/home.blade.php
@@ -13,13 +13,13 @@
 <section class="animal-products parent container-fluid py-5 bg">
     <span class="top spacer layer"></span>
   
-    <h3 class="d-flex justify-content-center align-items-center my-3">بەرهەمەکان</h3>
+    <h3 class="d-flex products-header justify-content-center align-items-center my-3">بەرهەمەکان</h3>
   
-    <div class="row text-center justify-content-center my-2 mx-0">
-      <a class="col my-5" href="#Foods"><img src="myImages/happy cat.webp" alt="happy cat" width="300px"height="300px"></a>
-      <a class="col my-5" href="#Foods"><img src="myImages/royal.jpg" alt="royal"width="300px"height="300px"></a>
-      <a class="col my-5" href="#Foods"><img src="myImages/bird.jpg" alt="bird"width="300px"height="300px"></a>
-      <a class="col my-5" href="#Foods"><img src="myImages/rabbit.jpg" alt="rabbit"width="300px"height="300px"></a>
+    <div class="products row text-center justify-content-center my-2 mx-0">
+      <a class="products-item col my-5" href="#Foods"><img class="products-image" src="myImages/happy cat.webp" alt="happy cat" width="300px"height="300px"></a>
+      <a class="products-item col my-5" href="#Foods"><img class="products-image" src="myImages/royal.jpg" alt="royal"width="300px"height="300px"></a>
+      <a class="products-item col my-5" href="#Foods"><img class="products-image" src="myImages/bird.jpg" alt="bird"width="300px"height="300px"></a>
+      <a class="products-item col my-5" href="#Foods"><img class="products-image" src="myImages/rabbit.jpg" alt="rabbit"width="300px"height="300px"></a>
     </div>
     <span class="bottom spacer layer2"></span>
 </section>
