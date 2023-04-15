7668a8e0678c338fae15edbff70da84eb2c11ba8
Yahya-Mewan <yahya.00228959@gmail.com>
2023-04-15 11:08:57 +0300

    stylizing

--- resources/views/pages/home/home.blade.php
+++ resources/views/pages/home/home.blade.php
@@ -3,37 +3,33 @@
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link href="css/home/caring.css" rel="stylesheet" type="text/css">
-<link rel="stylesheet" href="css/home/addImages.css">
-
-@include('header')
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="css/bootstrap/bootstrap.css">
+<link rel="stylesheet" href="css/home/products-section.css">
 </head>
 <body>
-
+  
+@include('header')
 @include('pages.home.slideShow')
-<br>
-<h3 style="text-align: center; font-size:30px">بەرهەمەکان</h3>
+<section class="animal-products parent container-fluid py-5 bg">
+    <span class="top spacer layer"></span>
+  
+    <h3 class="d-flex justify-content-center align-items-center my-3">بەرهەمەکان</h3>
+  
+    <div class="row text-center justify-content-center my-2 mx-0">
+      <a class="col my-5" href="#Foods"><img src="myImages/happy cat.webp" alt="happy cat" width="300px"height="300px"></a>
+      <a class="col my-5" href="#Foods"><img src="myImages/royal.jpg" alt="royal"width="300px"height="300px"></a>
+      <a class="col my-5" href="#Foods"><img src="myImages/bird.jpg" alt="bird"width="300px"height="300px"></a>
+      <a class="col my-5" href="#Foods"><img src="myImages/rabbit.jpg" alt="rabbit"width="300px"height="300px"></a>
+    </div>
+    <span class="bottom spacer layer2"></span>
+</section>
 
-<br>
-
-<div style="text-align: center">
-
-    <a href="#Foods"><img src="myImages/happy cat.webp" alt="happy cat" width="300px"height="300px"></a>
-    <a href="#Foods"><img src="myImages/royal.jpg" alt="royal"width="300px"height="300px"></a>
-    <a href="#Foods"><img src="myImages/bird.jpg" alt="bird"width="300px"height="300px"></a>
-    <a href="#Foods"><img src="myImages/rabbit.jpg" alt="rabbit"width="300px"height="300px"></a>
-
-  </div>
-<br><br><br>
 @include('pages.home.bigImage')
-
-<br><br><br><br>
-
+@include('pages.home.caring')
 @yield('content')
-<br><br><br><br><br>
-<script src="js/addImage.js"></script>
-
-</body>
-<br><br><br><br><br><br><br><br><br><br>
 @include('footer') 
-
+<script src="js/bootstrap/popper.min.js"></script>
+<script src="js/bootstrap/bootstrap.min.js"></script>
+</body>
 </html>
