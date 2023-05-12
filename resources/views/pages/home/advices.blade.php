cb808a012fd9efc079e76f07356fe1901d53fe5b
Majeed-Omer <twom764@gmail.com>
2023-05-12 05:56:50 +0300

    m

---
+++ resources/views/pages/home/advices.blade.php
@@ -0,0 +1,45 @@
+<section class="sectionAdvice">
+      <div class="rowAdvice">
+        <h2>ئاژەڵان پێویستییان بەچییە؟</h2>
+      </div>
+      <div class="rowAdvice">
+        <!-- Column One -->
+        <div class="columnAdvice">
+          <div class="cardAdvice">
+            <div class="icon">
+              <i class="fa-solid fa-paw"></i>
+            </div>
+            <h3>خواردنی تەندروست</h3>
+            <p>
+              یەکێک لەهۆکارە زۆر باوەکانی نەخۆشکەوتی ئاژەڵەکان خواردنی ناتەندروست و خراپی جۆرەکەیتی
+              بۆیە پێویستە ئەو خواردنانەی دەیدەین بەئاژەڵەکان لەژێر چاودێری پزیشکی پسپۆڕبێت            </p>
+          </div>
+        </div>
+        <!-- Column Two -->
+        <div class="columnAdvice">
+          <div class="cardAdvice">
+            <div class="icon">
+              <i class="fa-solid fa-home"></i>
+            </div>
+            <h3>جێگایەکی گونجاو</h3>
+            <p>
+              ئەو جێگایەی کە بۆ ئاژەڵەکانمان هەڵیدەبژێرین پێویستە پاکوخاوێن بێت و کەش وهەوایەکەی گونجاوبێت 
+              چونکە ئاژەڵان ناتوانن لەهەموو جۆرە کەشێک بەردەوامی بدەن بە ژیان 
+            </p>
+          </div>
+        </div>
+        <!-- Column Three -->
+        <div class="columnAdvice">
+          <div class="cardAdvice">
+            <div class="icon">
+              <i class="fa-solid fa-hand-holding-heart"></i>
+            </div>
+            <h3>بایەخدانی زۆر</h3>
+            <p>
+              لەسەر خاوەن ئاژەڵەکان پێویستە کە گرینگییەکی زۆر بەئاژەڵەکانییان بدەن و بیان پارێزن
+              پێویستە بەلایەنی کەم لە ساڵێک جارەک یاخود دوو جار بیانبەن بۆلای پزیشکی ئاژەڵی
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
--- resources/views/pages/home/home.blade.php
+++ resources/views/pages/home/home.blade.php
@@ -3,7 +3,10 @@
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>ماڵەوە</title>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<!--  -->
+    <!-- Stylesheet -->
+    <link rel="stylesheet" href="css/home/advices.css" />
+<!-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> -->
 
 <!-- Bootstrap CSS -->
 <link rel="stylesheet" href="css/bootstrap/bootstrap.css">
@@ -18,21 +21,9 @@
   
 @include('header')
 @include('pages.home.slideShow')
-<section class="animal-products parent container-fluid py-5 bg">
-    <span class="top spacer layer"></span>
-  
-    <h3 class="d-flex products-header justify-content-center align-items-center my-3">خۆراکەکان</h3>
-  
-    <div class="products row text-center justify-content-center my-2 mx-0">
-      <a class="products-item col my-5" href="/all_supply"><img class="products-image" src="myImages/happy cat.png" alt="happy cat" width="300px"height="300px"></a>
-      <a class="products-item col my-5" href="/all_supply"><img class="products-image" src="myImages/royal dog.webp" alt="royal"width="300px"height="300px"></a>
-      <a class="products-item col my-5" href="/all_supply"><img class="products-image" src="myImages/bird food.png" alt="bird"width="300px"height="300px"></a>
-      <a class="products-item col my-5" href="/all_supply"><img class="products-image" src="myImages/check food.png" alt="rabbit"width="300px"height="300px"></a>
-    </div>
-    <span class="bottom spacer layer2"></span>
-</section>
-
+@include('pages.home.products-section')
 @include('pages.home.bigImage')
+@include('pages.home.advices')    
 <br><br><br>
 @yield('content')
 <br><br>
