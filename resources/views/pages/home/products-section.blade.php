5a8928cf4d9000c848c19795d4997b3018da8700
Yahya-Mewan <yahya.00228959@gmail.com>
2023-05-13 00:39:41 +0300

    y

--- resources/views/pages/home/products-section.blade.php
+++ resources/views/pages/home/products-section.blade.php
@@ -4,29 +4,29 @@
     <h3 class="d-flex products-header justify-content-center align-items-center my-3">خۆراکەکان</h3>
   
     <div class="products row text-center justify-content-center my-2 mx-0">
-    <div class="card products-item col my-5 mx-3">
-    <div class="content">
+    <div class="product-card products-item col my-5 mx-3">
+    <div class="product-content">
       <h2 class="title">خواردنی پشیلە</h2>
       <p class="copy">کلیک لەسەر ئەم لینکەی خوارەوە بکە بۆ بینینی خواردنەکانی پشیلەکان</p>
       <button class="bton">کلیک لێرە بکە</button>
     </div>
   </div>
-  <div class="card products-item col my-5 mx-3">
-    <div class="content">
+  <div class="product-card products-item col my-5 mx-3">
+    <div class="product-content">
       <h2 class="title">خواردنی سەگ</h2>
       <p class="copy">کلیک لەسەر ئەم لینکەی خوارەوە بکە بۆ بینینی خواردنەکانی سەگەکان</p>
       <button class="bton">کلیک لێرە بکە</button>
     </div>
   </div>
-  <div class="card products-item col my-5 mx-3">
-    <div class="content">
+  <div class="product-card products-item col my-5 mx-3">
+    <div class="product-content">
       <h2 class="title">خواردنی باڵندە</h2>
       <p class="copy">کلیک لەسەر ئەم لینکەی خوارەوە بکە بۆ بینینی خواردنەکانی باڵندەکان</p>
       <button class="bton">کلیک لێرە بکە</button>
     </div>
   </div>
-  <div class="card products-item col my-5 mx-3">
-    <div class="content">
+  <div class="product-card products-item col my-5 mx-3">
+    <div class="product-content">
       <h2 class="title">خواردنی پەلەوەر</h2>
       <p class="copy">کلیک لەسەر ئەم لینکەی خوارەوە بکە بۆ بینینی خواردنەکانی پەلەوەرەکان</p>
       <button class="bton">بەم زووانە</button>
